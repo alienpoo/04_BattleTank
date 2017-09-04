c76bd236c03bd5be90d2c3b027eeb428398c8b1d
Alienpoo <hampsterpoo@gmail.com>
2017-09-04 15:44:54 -0500

    Setup Override for TankPlayerController in C++.  Added override begin play to start tick events and log possed tank to console.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -6,6 +6,22 @@
 
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (ControlledTank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController: %s"), *(ControlledTank->GetName()))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("PlayerController No tank found!"))
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
